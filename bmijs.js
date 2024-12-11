9586bc49224b28902c1f8a2af7ad1f0fdb8b7b9d
mohdmustaqeem7830 <mustakimraza38@gmail.com>
2024-12-11 11:45:10 +0530

    link updated

--- bmijs.js
+++ bmijs.js
@@ -61,7 +61,7 @@ form.addEventListener('submit',function(e){
     }
 
 again.addEventListener('click',function(e){
-    window.location.href = 'http://127.0.0.1:5500/';
+    location.reload();
 })
 
 
